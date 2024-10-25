aae41840bb3e9e78b033457e01f784deb5b5cd98
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-10-25 13:26:54 +0300

    Completed Task 1

---
+++ Task 1.py
@@ -0,0 +1,9 @@
+a = int(input('Enter the number of the day of the week (Monday - 1; Wednesday - 3 etc): '))
+
+if a == 1: print('The 1rd day of the week is Monday')
+elif a == 2: print('The 2rd day of the week is Tuesday')
+elif a == 3: print('The 3rd day of the week is Wednesday')
+elif a == 4: print('The 4th day of the week is Thursday')
+elif a == 5: print('The 5th day of the week is Friday')
+elif a == 6: print('The 6th day of the week is Saturday')
+else: print('The 7th day of the week is Sunday')
